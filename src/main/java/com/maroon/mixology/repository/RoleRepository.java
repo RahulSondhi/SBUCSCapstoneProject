63847ef5a79565b2852c91768d8aa1acf5e07f7d
Bryan <bryan.valarezo24@gmail.com>
2019-10-19 22:15:09 -0400

    Auto stash before rebase of "refs/heads/master"

--- src/main/java/com/maroon/mixology/repository/RoleRepository.java
+++ src/main/java/com/maroon/mixology/repository/RoleRepository.java
@@ -2,13 +2,10 @@ package com.maroon.mixology.repository;
  
 import com.maroon.mixology.entity.Role;
 import org.springframework.data.mongodb.repository.MongoRepository;
-<<<<<<< Updated upstream
+
 import org.springframework.stereotype.Repository;
 
 @Repository
-=======
-
->>>>>>> Stashed changes
 public interface RoleRepository extends MongoRepository<Role, String> {
 
     Role findByRole(String role);
