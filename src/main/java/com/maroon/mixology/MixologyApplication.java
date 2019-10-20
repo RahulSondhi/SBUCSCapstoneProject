f60693c4dc8598c60a73e95447518d0bd38832e9
Manny <Immanuel.Almosara@stonybrook.edu>
2019-10-20 17:15:11 -0400

    Changed html a little

--- src/main/java/com/maroon/mixology/MixologyApplication.java
+++ src/main/java/com/maroon/mixology/MixologyApplication.java
@@ -2,12 +2,33 @@ package com.maroon.mixology;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.maroon.mixology.entity.Role;
+import com.maroon.mixology.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class MixologyApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MixologyApplication.class, args);
 	}
-
+	
+	@Bean
+	CommandLineRunner init(RoleRepository roleRepository) {
+		
+		return args -> {
+			
+			Role userRole = roleRepository.findByRole("USER");
+			if (userRole == null) {
+				Role newUserRole = new Role();
+				newUserRole.setRole("USER");
+				roleRepository.save(newUserRole);
+			}
+		};
+		
+	}
 }
