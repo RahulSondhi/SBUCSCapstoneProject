f43acd895d0fb715591201f07f94006c1e76a02b
Judy <judychen92174@gmail.com>
2019-10-24 13:44:03 -0400

    root

--- src/main/resources/static/reactexample/src/components/Login.js
+++ src/main/resources/static/reactexample/src/components/Login.js
@@ -4,13 +4,12 @@ import '../css/login.css';
 class Login extends Component {
     render() {
         return(
-          <div>
-      <div class="container">
-          <div class="logo">
+      <div className="container">
+          <div className="logo">
               Mixology
           </div>
           <form method="post">
-              <div class="sign-in-form">
+              <div className="sign-in-form">
                   <h2>Login your account.</h2>
                   <div>
                       <div>Invalid username or
@@ -20,49 +19,15 @@ class Login extends Component {
                       <div>You have been logged out.</div>
                   </div>
                   <br/>
-                  <label for="username">Username</label>: <input type="username" id="username" name="username" autofocus="autofocus" placeholder="Username" />
+                  <label htmlFor="username">Username</label>: <input type="username" id="username" name="username" autoFocus="autofocus" placeholder="Username" />
                   <br/><br/>
-                  <label for="password">Password</label>: <input type="password" id="password" name="password" placeholder="Password" />
+                  <label htmlFor="password">Password</label>: <input type="password" id="password" name="password" placeholder="Password" />
 
                   <br/><br/>
-                  <input type="submit" name="login-submit" class="button" value="Log In" />
-                  <span class="button" id="myBtn">Register</span>
+                  <input type="submit" name="login-submit" className="button" value="Log In" />
+                  <span className="button" id="myBtn">Register</span>
               </div>
           </form>
-      </div>
-
-      <div id="myModal" class="modal">
-
-          <div class="modal-content">
-              <span class="close">&times;</span>
-              <form method="post">
-
-                  <br/><br/>
-                  <h2>Register your account.</h2>
-                  <br/><br/>
-
-                  <label for="username">Username</label>
-                  <input id="username" name="username" type="username" placeholder="Username" />
-
-                  <br/><br/>
-                  <label for="password">Password</label>
-                  <input id="password" name="password" type="password" placeholder="Password" />
-
-                  <br/><br/>
-                  <label for="passwordConfirm">Confirm password</label>
-                  <input id="passwordConfirm" name="passwordConfirm" type="password" placeholder="Confirm Password" />
-
-                  <div class="form-group">
-                      <br/>
-                      <button type="submit" class="button">Register</button>
-                      <br/>
-                      <span>Already registered? <a href="/">Login here</a></span>
-                  </div>
-
-              </form>
-          </div>
-
-      </div>
       </div>
     )
    }
