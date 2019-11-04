132c0b54cc13754763c9bad6472a58362ce9e1aa
Bryan <bryan.valarezo24@gmail.com>
2019-11-04 02:01:18 -0500

    Minor update to run script
    mixology -> tipsy

--- scripts/run-scripts/run.sh
+++ scripts/run-scripts/run.sh
@@ -2,5 +2,5 @@
 pushd ../../
 x-terminal-emulator -e "mvn clean package && java -jar target/*.jar" ;
 popd
-pushd ../../mixology-react/
+pushd ../../tipsy-react/
 x-terminal-emulator -e "npm install && npm start"
