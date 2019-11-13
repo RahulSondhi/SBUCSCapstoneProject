ad2ab52b2ffe1cb20eb0ed238c2185c3516190e9
Bryan <bryan.valarezo24@gmail.com>
2019-11-13 08:27:16 -0500

    Added Equipment Objects in the database
    Please import the new tipsy-dump to get Equipment

--- src/main/java/com/maroon/mixology/MixologyApplication.java
+++ src/main/java/com/maroon/mixology/MixologyApplication.java
@@ -5,11 +5,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.maroon.mixology.entity.Bar;
+import com.maroon.mixology.entity.Equipment;
+import com.maroon.mixology.entity.type.ActionType;
+import com.maroon.mixology.entity.type.EquipmentType;
+
 import com.maroon.mixology.entity.Role;
 import com.maroon.mixology.entity.User;
 import com.maroon.mixology.entity.type.MeasurementType;
 import com.maroon.mixology.entity.Recipe;
 import com.maroon.mixology.repository.BarRepository;
+import com.maroon.mixology.repository.EquipmentRepository;
 import com.maroon.mixology.repository.RoleRepository;
 import com.maroon.mixology.repository.UserRepository;
 
@@ -27,9 +32,38 @@ public class MixologyApplication {
     
     
 	@Bean
-    CommandLineRunner init(UserRepository userRepository, BarRepository barRepository, RoleRepository roleRepository) {
+    CommandLineRunner init(UserRepository userRepository, BarRepository barRepository, RoleRepository roleRepository, EquipmentRepository equipmentRepository) {
         return args -> {
-
+            // Equipment e = new Equipment();
+            // e.setName("Simple Syrup");
+            // e.setType(EquipmentType.SYRUP);
+            //Actions
+            // Set<ActionType> doTo = new HashSet<ActionType>();
+            // Set<ActionType> doing = new HashSet<ActionType>();
+            // Add image
+            // e.setImage("");
+            // Add actions
+            // DoTo
+            // doTo.add(ActionType.POUR);
+            // doTo.add(ActionType.ADD);
+            // doTo.add(ActionType.MIX);
+            // doTo.add(ActionType.SHAKE);
+            // doTo.add(ActionType.MEASURE);
+            // Doing
+            // doing.add(ActionType.POUR);
+            // doing.add(ActionType.SHAKE);
+            // doing.add(ActionType.PRESS);
+            // doing.add(ActionType.CRUSH);
+            // doing.add(ActionType.CUT);
+            // doing.add(ActionType.MEASURE);
+            // doing.add(ActionType.BLEND);
+            // doing.add(ActionType.PEEL);
+            // doing.add(ActionType.HEAT);
+            // doing.add(ActionType.COOL);
+            //
+            // e.setActionsDoTo(doTo);
+            // e.setActionsDoing(doing);
+            // equipmentRepository.save(e);
             // // Role adminRole = roleRepository.findByName("ADMIN");
             // // if (adminRole == null) {
             // //     Role newUserRole = new Role();
