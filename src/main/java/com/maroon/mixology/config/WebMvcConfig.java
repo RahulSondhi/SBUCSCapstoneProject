e04e53db77dcd3691a5d39f994c212beea202a1f
Bryan <bryan.valarezo24@gmail.com>
2019-12-11 03:54:21 -0500

    Lets hope this fixes CORS

--- src/main/java/com/maroon/mixology/config/WebMvcConfig.java
+++ src/main/java/com/maroon/mixology/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/Tipsy/**")
+        registry.addMapping("*")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST")
                 .maxAge(MAX_AGE_SECS);
