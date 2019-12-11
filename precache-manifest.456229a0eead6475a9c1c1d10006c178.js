bdffc0cf8f802f28276f2edaf9ad6b9564bb9ac8
Bryan <bryan.valarezo24@gmail.com>
2019-12-11 01:38:41 -0500

    Updates

---
+++ precache-manifest.456229a0eead6475a9c1c1d10006c178.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26cafd0975707c606aae76de56685d8",
+    "url": "/Tipsy/index.html"
+  },
+  {
+    "revision": "c131ff6c28190df4c540",
+    "url": "/Tipsy/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "a65fc264226ddb601adf",
+    "url": "/Tipsy/static/css/main.cd495e84.chunk.css"
+  },
+  {
+    "revision": "c131ff6c28190df4c540",
+    "url": "/Tipsy/static/js/2.583bb6c7.chunk.js"
+  },
+  {
+    "revision": "a65fc264226ddb601adf",
+    "url": "/Tipsy/static/js/main.580b0b86.chunk.js"
+  },
+  {
+    "revision": "3ad6b658d8703ff8e813",
+    "url": "/Tipsy/static/js/runtime-main.2c8d7ed5.js"
+  },
+  {
+    "revision": "0e44381f3d199527ef87ac558f8458ac",
+    "url": "/Tipsy/static/media/Tipsy.0e44381f.png"
+  },
+  {
+    "revision": "c24232103f17b4c02420cea497d5f55d",
+    "url": "/Tipsy/static/media/Tipsy.c2423210.svg"
+  },
+  {
+    "revision": "b585cb913e051ace37dddccb4b68b279",
+    "url": "/Tipsy/static/media/action.b585cb91.svg"
+  },
+  {
+    "revision": "311313cded0dc23bba479b786ac0aad8",
+    "url": "/Tipsy/static/media/add.311313cd.svg"
+  },
+  {
+    "revision": "00c0640d71751ef64585513d33253311",
+    "url": "/Tipsy/static/media/bar.00c0640d.svg"
+  },
+  {
+    "revision": "70ae5599512fe1c27bf4a545b56e5dcb",
+    "url": "/Tipsy/static/media/bg.70ae5599.jpg"
+  },
+  {
+    "revision": "8c6deffd41bb1abb0910f64d8a504197",
+    "url": "/Tipsy/static/media/bottle.8c6deffd.svg"
+  },
+  {
+    "revision": "153f02c39ed38bb7ac5b4cc32b2eab4c",
+    "url": "/Tipsy/static/media/broken_glass.153f02c3.svg"
+  },
+  {
+    "revision": "e252e99689bd8de3a2ee7f9ea26a2c2e",
+    "url": "/Tipsy/static/media/broken_link.e252e996.svg"
+  },
+  {
+    "revision": "a196e03e08b23b21dd0ca9474f56e9a4",
+    "url": "/Tipsy/static/media/card.a196e03e.jpg"
+  },
+  {
+    "revision": "60a0367f6cb0fb4185f28008c07f2ec0",
+    "url": "/Tipsy/static/media/clone.60a0367f.svg"
+  },
+  {
+    "revision": "f202fe5b29e10919c8b5e5a38f398301",
+    "url": "/Tipsy/static/media/cup.f202fe5b.svg"
+  },
+  {
+    "revision": "44d672f80bb21b1a5254fa08a585cb75",
+    "url": "/Tipsy/static/media/down.44d672f8.svg"
+  },
+  {
+    "revision": "5c79fd19ccff4bcf0ada1f6aa641e6c9",
+    "url": "/Tipsy/static/media/drinks.5c79fd19.svg"
+  },
+  {
+    "revision": "47adca0a9c43846e9c13bff391a4e29f",
+    "url": "/Tipsy/static/media/equipment.47adca0a.svg"
+  },
+  {
+    "revision": "48338660b75bbf9828c9a0c8943c7e83",
+    "url": "/Tipsy/static/media/fzshuti.48338660.ttf"
+  },
+  {
+    "revision": "9a315467a2fafadaf7ab32db4bbd4669",
+    "url": "/Tipsy/static/media/metalPlate.9a315467.jpg"
+  },
+  {
+    "revision": "fe4dc2fc6442bf2b021c4ae70991269d",
+    "url": "/Tipsy/static/media/play.fe4dc2fc.svg"
+  },
+  {
+    "revision": "fcf48b958caa4cdad337bf2eb4647e36",
+    "url": "/Tipsy/static/media/recipe.fcf48b95.svg"
+  },
+  {
+    "revision": "971f63595c1c24bb750198aefd9fad40",
+    "url": "/Tipsy/static/media/remove.971f6359.svg"
+  },
+  {
+    "revision": "563d4156f2f86d3d9a16edd6a84ecda3",
+    "url": "/Tipsy/static/media/search.563d4156.svg"
+  },
+  {
+    "revision": "3a076725cde792077c9dfe1713ca0ac4",
+    "url": "/Tipsy/static/media/setting.3a076725.svg"
+  },
+  {
+    "revision": "b500910577e4c25826fde679839fbd15",
+    "url": "/Tipsy/static/media/unknown.b5009105.svg"
+  },
+  {
+    "revision": "889bcc615248100869196f60d1ac840a",
+    "url": "/Tipsy/static/media/up.889bcc61.svg"
+  },
+  {
+    "revision": "7a21cbe90fd31540fb0384792fbc4a86",
+    "url": "/Tipsy/static/media/user.7a21cbe9.svg"
+  }
+]);
